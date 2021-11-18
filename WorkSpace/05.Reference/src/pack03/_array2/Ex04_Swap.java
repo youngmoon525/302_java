2c1171ee190fbfc146f6978cc8c33dd6ae8aac25
youngmoon1 <youngmoun525@gmail.com>
2021-11-18 13:08:50 +0900

    20211118 예외 처리

--- WorkSpace/05.Reference/src/pack03/_array2/Ex04_Swap.java
+++ WorkSpace/05.Reference/src/pack03/_array2/Ex04_Swap.java
@@ -1,5 +1,7 @@
 package pack03._array2;
 
+import java.util.Arrays;
+
 public class Ex04_Swap {
 	public static void main(String[] args) {
 		int a = 10; 
@@ -17,8 +19,19 @@ public class Ex04_Swap {
 		// swap이라는 개념을 이용해서 1차원 배열에 들어있는 int값들을
 		// 오름 차순으로 정렬 하는 프로그램을 작성하세요.
 		// 어떤 식으로 해도 상관은 없습니다. 
-		
-		
+		// 
+		//{ 3 , 5 ,7 , 9 , 1 , 2 , 4 , 10};
+		int[] arrm = { 3,4,6,7,8,9,1,2,5};
+		for (int i = 0; i < arrm.length; i++) {
+			for (int j = i+1; j < arrm.length+2; j++) {
+				if(arrm[i] > arrm[j]) {
+					int tmp = arrm[i];
+					arrm[i] = arrm[j];
+					arrm[j] = tmp;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(arrm));
 		
 		
 		
